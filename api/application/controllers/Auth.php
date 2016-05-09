309d348947449ea6e83796d469dab149af0f0a1a
Nhan Cao <nhancv1992@gmail.com>
2016-05-09 17:45:16 +0700

    fix mvc update file .htaccess

--- api/application/controllers/Auth.php
+++ api/application/controllers/Auth.php
@@ -4,14 +4,19 @@
  * Created by IntelliJ IDEA.
  * User: nhancao
  * Date: 5/9/16
- * Time: 12:45 PM
+ * Time: 5:36 PM
  */
-class Main extends Controller
-{
+class Auth extends Controller {
+
     function index()
     {
         $template = $this->loadView('main_view');
         $template->render();
+    }
+
+    function login()
+    {
+        echo 'Hello World!';
     }
 
 }
