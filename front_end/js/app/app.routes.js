24b6861c50056d945d60607e3cc288962e75237e
Nhan Cao <nhancv1992@gmail.com>
2016-05-18 01:44:55 +0700

    multiple & name view change example

--- front_end/js/app/app.routes.js
+++ front_end/js/app/app.routes.js
@@ -33,7 +33,33 @@ angular.module('app.routes', [])
                 controller: function($scope) {
                     $scope.things = ["A", "Set", "Of", "Things"];
                 }
-            });
+            })
+        //Multiple & name views
+            .state('index', {
+                url: "",
+                views: {
+                    "viewA": { template: "index.viewA" },
+                    "viewB": { template: "index.viewB" }
+                }
+            })
+            .state('route1', {
+                url: "/route1",
+                views: {
+                    "viewA": { template: "route1.viewA" },
+                    "viewB": { template: "route1.viewB" }
+                }
+            })
+            .state('route2', {
+                url: "/route2",
+                views: {
+                    "viewA": { template: "route2.viewA" },
+                    "viewB": { template: "route2.viewB" }
+                }
+            })
+
+
+
+        ;
     })
 
 
