5636e11b59b777af1a1a3a00470358670c2d1385
Nhan Cao <nhancv1992@gmail.com>
2016-05-18 22:38:23 +0700

    create nc-body directive

--- front_end/js/app/app.directives.js
+++ front_end/js/app/app.directives.js
@@ -2,6 +2,12 @@
  * Created by nhancao on 5/18/16.
  */
 angular.module('app.directives', [])
+    .directive('ncBody', function () {
+        return {
+            restrict: 'EA',
+            template: '    <div ng-include="\'front_end/partials/body.html\'" style="min-height: calc(100vh - 128px); max-height: 700px;"></div>'
+        };
+    })
     .directive('blankDirective', function () {
 
     })
