260c8434306cff2df025f0af6ea5bdfc24ed38c3
Nhan Cao <nhancv1992@gmail.com>
2016-05-25 23:01:31 +0700

    update tabs

--- front_end/js/app/app.controllers.js
+++ front_end/js/app/app.controllers.js
@@ -55,7 +55,34 @@ angular.module('app.controllers', [])
     .controller('cBody', function ($scope, $timeout, $mdSidenav, $mdDialog, $log) {
 
     })
-
+    .controller('cUser', function ($scope, $log) {
+        var tabs = [
+                {title: 'Home', content: "<h1>Test</h1>Tabs will become paginated if there isn't enough room for them."},
+                {title: 'Add', content: "You can swipe left and right on a mobile device to change tabs."},
+                {
+                    title: 'Setting',
+                    content: "You can bind the selected tab via the selected attribute on the md-tabs element."
+                }
+            ],
+            selected = null,
+            previous = null;
+        $scope.tabs = tabs;
+        $scope.selectedIndex = 0;
+        $scope.$watch('selectedIndex', function (current, old) {
+            previous = selected;
+            selected = tabs[current];
+            if (old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
+            if (current + 1)                $log.debug('Hello ' + selected.title + '!');
+        });
+        $scope.addTab = function (title, view) {
+            view = view || title + " Content View";
+            tabs.push({title: title, content: view, disabled: false});
+        };
+        $scope.removeTab = function (tab) {
+            var index = tabs.indexOf(tab);
+            tabs.splice(index, 1);
+        };
+    })
     .controller('cBlank', function ($scope) {
 
     })
