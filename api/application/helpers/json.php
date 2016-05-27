48cd07e71a45ed8469093674ace863447d5e615f
Nhan Cao <nhancv1992@gmail.com>
2016-05-27 21:29:39 +0700

    update commit json

--- api/application/helpers/json.php
+++ api/application/helpers/json.php
@@ -8,12 +8,12 @@
  */
 class Json
 {
-    function json_encode_utf8($struct)
+    public static function json_encode_utf8($struct)
     {
         return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
     }
 
-    function json_decode_nice($json, $assoc = FALSE)
+    public static function json_decode_nice($json, $assoc = FALSE)
     {
         $json = str_replace(array("\n", "\r"), "", $json);
         $json = preg_replace('/([{,]+)(\s*)([^"]+?)\s*:/', '$1"$3":', $json);
