3e0e91b053cfa60f7bff0e067ad7142e1a810e1b
Nhan Cao <nhancv1992@gmail.com>
2016-05-28 13:16:06 +0700

    get user

--- front_end/js/app/app.controllers.js
+++ front_end/js/app/app.controllers.js
@@ -59,9 +59,6 @@ angular.module('app.controllers', [])
     .controller('cUser', function ($rootScope, $scope, $mdDialog, $mdEditDialog, $q, $timeout, $log, sApiCall) {
         'use strict';
 
-        sApiCall.getAllUser().then(function (results) {
-            console.log(results);
-        });
 
         var newUser = {
             "uid": "3",
@@ -80,9 +77,9 @@ angular.module('app.controllers', [])
         //sApiCall.insertUser(newUser).then(function (results) {
         //    console.log(results);
         //});
-        sApiCall.updateUser(newUser).then(function (results) {
-            console.log(results);
-        });
+        //sApiCall.updateUser(newUser).then(function (results) {
+        //    console.log(results);
+        //});
         //sApiCall.deleteUser(newUser).then(function (results) {
         //    console.log(results);
         //});
@@ -128,7 +125,7 @@ angular.module('app.controllers', [])
         $scope.options = {
             rowSelection: true,
             multiSelect: true,
-            autoSelect: true,
+            autoSelect: false,
             decapitate: false,
             largeEditDialog: false,
             boundaryLinks: true,
@@ -171,102 +168,7 @@ angular.module('app.controllers', [])
                 templateUrl: 'front_end/partials/delete-dialog.html',
             }).then($scope.users);
         };
-        $scope.users = {
-            "count": 9,
-            "data": [
-                {
-                    "name": "Frozen yogurt",
-                    "type": "Ice cream",
-                    "calories": {"value": 159.0},
-                    "fat": {"value": 6.0},
-                    "carbs": {"value": 24.0},
-                    "protein": {"value": 4.0},
-                    "sodium": {"value": 87.0},
-                    "calcium": {"value": 14.0},
-                    "iron": {"value": 1.0}
-                }, {
-                    "name": "Ice cream sandwich",
-                    "type": "Ice cream",
-                    "calories": {"value": 237.0},
-                    "fat": {"value": 9.0},
-                    "carbs": {"value": 37.0},
-                    "protein": {"value": 4.3},
-                    "sodium": {"value": 129.0},
-                    "calcium": {"value": 8.0},
-                    "iron": {"value": 1.0}
-                }, {
-                    "name": "Eclair",
-                    "type": "Pastry",
-                    "calories": {"value": 262.0},
-                    "fat": {"value": 16.0},
-                    "carbs": {"value": 24.0},
-                    "protein": {"value": 6.0},
-                    "sodium": {"value": 337.0},
-                    "calcium": {"value": 6.0},
-                    "iron": {"value": 7.0}
-                }, {
-                    "name": "Cupcake",
-                    "type": "Pastry",
-                    "calories": {"value": 305.0},
-                    "fat": {"value": 3.7},
-                    "carbs": {"value": 67.0},
-                    "protein": {"value": 4.3},
-                    "sodium": {"value": 413.0},
-                    "calcium": {"value": 3.0},
-                    "iron": {"value": 8.0}
-                }, {
-                    "name": "Jelly bean",
-                    "type": "Candy",
-                    "calories": {"value": 375.0},
-                    "fat": {"value": 0.0},
-                    "carbs": {"value": 94.0},
-                    "protein": {"value": 0.0},
-                    "sodium": {"value": 50.0},
-                    "calcium": {"value": 0.0},
-                    "iron": {"value": 0.0}
-                }, {
-                    "name": "Lollipop",
-                    "type": "Candy",
-                    "calories": {"value": 392.0},
-                    "fat": {"value": 0.2},
-                    "carbs": {"value": 98.0},
-                    "protein": {"value": 0.0},
-                    "sodium": {"value": 38.0},
-                    "calcium": {"value": 0.0},
-                    "iron": {"value": 2.0}
-                }, {
-                    "name": "Honeycomb",
-                    "type": "Other",
-                    "calories": {"value": 408.0},
-                    "fat": {"value": 3.2},
-                    "carbs": {"value": 87.0},
-                    "protein": {"value": 6.5},
-                    "sodium": {"value": 562.0},
-                    "calcium": {"value": 0.0},
-                    "iron": {"value": 45.0}
-                }, {
-                    "name": "Donut",
-                    "type": "Pastry",
-                    "calories": {"value": 452.0},
-                    "fat": {"value": 25.0},
-                    "carbs": {"value": 51.0},
-                    "protein": {"value": 4.9},
-                    "sodium": {"value": 326.0},
-                    "calcium": {"value": 2.0},
-                    "iron": {"value": 22.0}
-                }, {
-                    "name": "KitKat",
-                    "type": "Candy",
-                    "calories": {"value": 518.0},
-                    "fat": {"value": 26.0},
-                    "carbs": {"value": 65.0},
-                    "protein": {"value": 7.0},
-                    "sodium": {"value": 54.0},
-                    "calcium": {"value": 12.0},
-                    "iron": {"value": 6.0}
-                }
-            ]
-        };
+        $scope.users = {};
 
         $scope.removeFilter = function () {
             $scope.filter.show = false;
@@ -329,15 +231,17 @@ angular.module('app.controllers', [])
             return ['Normal', 'Block', 'Wait accept'];
         };
 
-        $scope.loadStuff = function () {
-            $scope.promise = $timeout(function () {
-                // loading
-            }, 2000);
-        },
+        $scope.loadUserList = function () {
+            $scope.promise =
+                sApiCall.getAllUser().then(function (results) {
+                    $scope.users = results;
+                    console.log($scope.users);
+                });
+        };
 
-            $scope.logItem = function (item) {
-                console.log(item.name, 'was selected');
-            };
+        $scope.logItem = function (item) {
+            console.log(item.name, 'was selected');
+        };
 
         $scope.logOrder = function (order) {
             console.log('order: ', order);
