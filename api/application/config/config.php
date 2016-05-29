5d5aae33e136ef21988810f826a151555fa478a2
Nhan Cao <nhancv1992@gmail.com>
2016-05-29 15:21:20 +0700

    deploy

--- api/application/config/config.php
+++ api/application/config/config.php
@@ -10,7 +10,7 @@ $config['base_url'] = ''; // Base URL including trailing slash (e.g. http://loca
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
-$config['db_host'] = 'localhost'; // Database host (e.g. localhost)
+$config['db_host'] = '52.221.225.183'; // Database host (e.g. localhost)
 $config['db_name'] = 'stock_girldayshop'; // Database name
 $config['db_username'] = 'admin'; // Database username
 $config['db_password'] = '123Cfvaojd#(@22(!s'; // Database password
